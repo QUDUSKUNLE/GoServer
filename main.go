9ee88394bdacc00161b2879324327a3a78c88350
QUDUSKUNLE <quduskunle@gmail.com>
2023-06-07 10:58:55 +0100

    Build simple API

--- main.go
+++ main.go
@@ -1,58 +1,87 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
-	"github.com/julienschmidt/httprouter"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
-	router := httprouter.New()
-	router.GET("/", HomeHandler)
+	router := gin.Default()
 
-	// Post collections
-	router.GET("/posts", PostsIndexHandler)
-	router.POST("/posts", PostsCreateHandler)
+	router.GET("/", home)
+	router.GET("/books", findBooks)
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
 
-	// Posts singular
-	router.GET("/posts/:id", PostsShowHandler)
-	router.PUT("/posts/:id", PostsUpdateHandler)
-	router.GET("/posts/:id/edit", PostsEditHandler)
-
-	fmt.Println("Serving running on port: "+port)
-	http.ListenAndServe("localhost:"+port, router)
+	router.Run("localhost:"+port)
 }
 
-func HomeHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	fmt.Fprintln(res, "Home")
+func postAlbums(cnx *gin.Context) {
+	var newAlbum album
+	if err := cnx.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+	cnx.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-func PostsIndexHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	fmt.Fprintln(res, "Posts index")
+func getAlbums(cnx *gin.Context) {
+	cnx.IndentedJSON(http.StatusOK, albums)
 }
 
-func PostsCreateHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	fmt.Fprintln(res, "Creates index")
+func getAlbumByID(cnx *gin.Context) {
+	id := cnx.Param("id")
+
+ 	// res, _ := HashPassword(id)
+	// fmt.Println(res)
+
+	for _, album := range albums {
+		if album.ID == id {
+			cnx.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+	cnx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
-func PostsShowHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	id := router.ByName("id")
-	fmt.Fprintln(res, "showing post", id)
+func home(cnx *gin.Context) {
+	cnx.IndentedJSON(http.StatusOK, "Home")
 }
 
-func PostsUpdateHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	fmt.Fprintln(res, "Post update")
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
 
-func PostsDeleteHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	fmt.Fprintln(res, "Post delete")
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
-func PostsEditHandler(res http.ResponseWriter, req *http.Request, router httprouter.Params) {
-	fmt.Fprintln(res, "Post edit")
+// GET /books
+// GET all books
+func findBooks(cg *gin.Context) {
+	cg.IndentedJSON(http.StatusOK, gin.H{"data": albums})
 }
--- models/book.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Book struct {
-	ID      uint `json:"id" gorm:"primary-key"`
-	Title   string `json:"title"`
-	Author  string `json:"author"`
-}
