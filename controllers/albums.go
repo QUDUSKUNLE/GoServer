2e3d79bfd3d260764ce0f1d2d830eb20263728a2
QUDUSKUNLE <quduskunle@gmail.com>
2023-06-07 12:33:31 +0100

    Clean up Go API

---
+++ controllers/albums.go
@@ -0,0 +1,55 @@
+package controllers
+
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"server/models"
+)
+
+// albums slice to seed record album data.
+var albums = []models.Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func PostAlbums(cnx *gin.Context) {
+	var newAlbum models.Album
+	if err := cnx.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+	cnx.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func GetAlbums(cnx *gin.Context) {
+	cnx.IndentedJSON(http.StatusOK, albums)
+}
+
+func GetAlbumByID(cnx *gin.Context) {
+	id := cnx.Param("id")
+
+	for _, album := range albums {
+		if album.ID == id {
+			cnx.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+	cnx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func Home(cnx *gin.Context) {
+	cnx.IndentedJSON(http.StatusOK, gin.H{"mesage": "Welcome to Go API"})
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
--- main.go
+++ main.go
@@ -1,25 +1,19 @@
 package main
 
 import (
-	"net/http"
 	"os"
+	"log"
+	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
+	"server/models"
+	"server/controllers"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+			log.Fatal("Error loading .env file")
+	}
 }
 
 func main() {
@@ -29,59 +23,12 @@ func main() {
 	}
 	router := gin.Default()
 
-	router.GET("/", home)
-	router.GET("/books", findBooks)
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/", controllers.Home)
+	router.GET("/books", controllers.FindBooks)
+	router.GET("/albums", controllers.GetAlbums)
+	router.GET("/albums/:id", controllers.GetAlbumByID)
+	router.POST("/albums", controllers.PostAlbums)
 
+	models.ConnectDatabase()
 	router.Run("localhost:"+port)
-}
-
-func postAlbums(cnx *gin.Context) {
-	var newAlbum album
-	if err := cnx.BindJSON(&newAlbum); err != nil {
-		return
-	}
-	albums = append(albums, newAlbum)
-	cnx.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-func getAlbums(cnx *gin.Context) {
-	cnx.IndentedJSON(http.StatusOK, albums)
-}
-
-func getAlbumByID(cnx *gin.Context) {
-	id := cnx.Param("id")
-
- 	// res, _ := HashPassword(id)
-	// fmt.Println(res)
-
-	for _, album := range albums {
-		if album.ID == id {
-			cnx.IndentedJSON(http.StatusOK, album)
-			return
-		}
-	}
-	cnx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-func home(cnx *gin.Context) {
-	cnx.IndentedJSON(http.StatusOK, "Home")
-}
-
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
-
-// GET /books
-// GET all books
-func findBooks(cg *gin.Context) {
-	cg.IndentedJSON(http.StatusOK, gin.H{"data": albums})
 }
