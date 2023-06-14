a0e6ce0d28acecbeca7dfc9d43fc31e9edf395ca
Abdul-Quddus Yekeen <quduskunle@gmail.com>
2023-06-14 06:37:14 +0100

    clean up order api (#7)

--- controllers/orders.go
+++ controllers/orders.go
@@ -1,75 +1,79 @@
 package controllers
 
 import (
-	// "encoding/json"
-	// "io"
+	"encoding/json"
+	"io"
 	"net/http"
 	"server/helpers"
 	"server/models"
 	"github.com/gin-gonic/gin"
-	"fmt"
 )
 
 func AddOrder(context *gin.Context) {
-	// var stocks []models.Stock
-	// var products []models.Product
-	// var productInput models.ProductInput
-	// var stockIDs []string
+	var stocks []models.Stock
+	var products []models.Product
+	var productInput models.ProductInput
+	var stockIDs []string
 	user, err := helpers.CurrentUser(context)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
 		return
 	}
-	fmt.Println(user)
 
-	// jsonData, _ := io.ReadAll(context.Request.Body)
-	// var orderInput models.OrderInputs
-	// if err := json.Unmarshal(jsonData, &orderInput); err != nil {
-	// 	context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
-	// }
-	// var stock models.Stock
-	// orderedProducts := make(map[string]models.ProductInput)
-	// if len(orderInput.Products) >= 1 {
-	// 	for _, product := range orderInput.Products {
-	// 		stockIDs = append(stockIDs, (product.StockID).String())
-	// 		var productInput = models.ProductInput{
-	// 			Quantity: product.Quantity,
-	// 			StockID: product.StockID,
-	// 		}
-	// 		orderedProducts[(product.StockID).String()] = productInput
-	// 	}
-	// 	stocks = stock.FindIn(stockIDs)
+	jsonData, _ := io.ReadAll(context.Request.Body)
+	var orderInput models.OrderInputs
+	if err := json.Unmarshal(jsonData, &orderInput); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
+		return
+	}
 
-	// 	if len(stocks) >= 1  {
-	// 		for _, stock := range stocks {
-	// 			productInput = orderedProducts[(stock.ID).String()]
-	// 			productInput.Stock = stock
-	// 			product := models.Product{
-	// 				Quantity: productInput.Quantity,
-	// 				Stock: stock,
-	// 				StockID: stock.ID,
-	// 			}
-	// 			products = append(products, product)
-	// 		}
-	// 	}
+	if err := context.ShouldBindJSON(&orderInput); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var stock models.Stock
+	orderedProducts := make(map[string]models.ProductInput)
+	if len(orderInput.Products) >= 1 {
+		for _, product := range orderInput.Products {
+			stockIDs = append(stockIDs, (product.StockID).String())
+			var productInput = models.ProductInput{
+				Quantity: product.Quantity,
+				StockID: product.StockID,
+			}
+			orderedProducts[(product.StockID).String()] = productInput
+		}
+		stocks = stock.FindIn(stockIDs)
 
-	// 	if err != nil {
-	// 		context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
-	// 		return
-	// 	}
-	// 	order := models.Order{
-	// 		Quantity: len(orderInput.Products),
-	// 		Products: products,
-	// 		UserID: user.ID,
-	// 	}
-	// 	savedOrder, err := order.Save()
-	// 	if err != nil {
-	// 		context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
-	// 		return
-	// 	}
-	// 	context.JSON(http.StatusCreated, gin.H{ "data":  savedOrder })
-	// 	return
-	// }
+		if len(stocks) >= 1  {
+			for _, stock := range stocks {
+				productInput = orderedProducts[(stock.ID).String()]
+				productInput.Stock = stock
+				product := models.Product{
+					Quantity: productInput.Quantity,
+					Stock: stock,
+					StockID: stock.ID,
+				}
+				products = append(products, product)
+			}
+		}
+
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
+			return
+		}
+		order := models.Order{
+			Quantity: len(orderInput.Products),
+			Products: products,
+			UserID: user.ID,
+		}
+		savedOrder, err := order.Save()
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
+			return
+		}
+		context.JSON(http.StatusCreated, gin.H{ "data":  savedOrder })
+		return
+	}
 	context.JSON(http.StatusBadRequest, gin.H{ "message": "No order made" })
 }
 
